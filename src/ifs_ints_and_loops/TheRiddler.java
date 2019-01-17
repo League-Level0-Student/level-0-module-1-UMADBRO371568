4c98dced21a8bb8107b5a576813ad7768bcc7aed
UMADBRO371568 <45644068+UMADBRO371568@users.noreply.github.com>
2019-01-16 19:32:11 -0800

    the riddler half complete

--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -1,25 +1,27 @@
 package ifs_ints_and_loops;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
+
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
-		// 1. Make a variable to hold the score
+		int score = 0;
 
-		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
-		// 5. Otherwise, say "wrong" and tell them the answer
-
-		// 6. Add some more riddles
-
-		// 2. Make a pop up to show the score.
+		String response = JOptionPane.showInputDialog("How old are you");
+		System.out.println(response);
+		if (response.equals("14")) {
+			System.out.println(("correct"));
+			score = score + 1;
+			System.out.println(score);
+		} else {
+			System.out.println("wrong");
+			System.out.println("The correct answer is 14");
+		}
 		
-	}
+		response=JOptionPane.showInputDialog("Who is the presedent of the United States");
 }
-
+}
